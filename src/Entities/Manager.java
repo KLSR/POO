4ed88e44ae1c4a6f7d8ddf76448fb33c3df7670d
KLSR <rslk0325@gmail.com>
2017-05-21 08:09:43 -0600

    no hice nada en Library.java

---
+++ src/Entities/Manager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Marco
+ */
+public class Manager extends User{//Create Manager
+    public Manager(String name,String email,String age,String user,String password,Calendar date){
+        super.setName(name);
+        super.setEmail(email);
+        super.setAge(age);
+        super.setUser(user);
+        super.setPassword(password);
+        super.setDate(date);
+    }
+}
